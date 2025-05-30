4bf8632df05753eac316125d3e23bf39102ac336
Beks <b_abdimanapov@u.pacific.edu>
2025-05-29 17:06:34 -0700

    the dinamic recording works

--- base.py
+++ base.py
@@ -4,22 +4,23 @@ import os
 from modules import voice_input as recording
 import whisper
 
-
+#main file
 if __name__ == "__main__":
+    #call for a record
     recording.record_audio("recording.wav")
     
     filename = "recording.wav"
+    
     if os.path.exists(filename):
         print(f"{filename} exists.")
         # Transcribe the audio
         model = whisper.load_model("base")
         result = model.transcribe(filename)
-        print(result["text"])
-        #text = result["text"].strip()
-        #print("Recognized words:", text)
-        # Write recognized words to audiototext.txt
-       #with open("audiototext.txt", "w", encoding="utf-8") as f:
-        #    f.write(text)
-        #print("Transcription saved to audiototext.txt")
+        text = result["text"].strip()
+        print("Recognized words:", text)
+        #Write recognized words to audiototext.txt
+        with open("audiototext.txt", "w", encoding="utf-8") as f:
+            f.write(text)
+        print("Transcription saved to audiototext.txt")
     else:
         print(f"{filename} does not exist.")
