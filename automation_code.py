af57a8cc89b4c9743d557c9d96e1799c49c2d501
Beks <b_abdimanapov@u.pacific.edu>
2025-06-16 01:24:47 -0700

    it opens stuff, still bugged tho

--- automation_code.py
+++ automation_code.py
@@ -1,9 +1,7 @@
-Here's a Python script using pyautogui to open YouTube in a web browser:
-
 import pyautogui
 import time
-import webbrowser
 
-webbrowser.open('https://www.youtube.com')
-time.sleep(3)
-pyautogui.click(x=100, y=100)  # Click on browser window to focus
+pyautogui.hotkey('ctrl', 't')
+time.sleep(1)
+pyautogui.typewrite('https://www.youtube.com/results?search_query=CSGO')
+pyautogui.press('enter')
