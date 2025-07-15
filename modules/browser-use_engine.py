0d0f21ee0fa842ce9151b029b4909a7334f84dfc
Beks <b_abdimanapov@u.pacific.edu>
2025-07-14 21:22:28 -0700

    new engine

---
+++ modules/browser-use_engine.py
@@ -0,0 +1,20 @@
+# Browser-use_engine - This module is used to control the browser using voice commands!
+from browser_use import Agent, Browser
+from browser_use.llm import ChatOpenAI
+
+class Engine:
+    def __init__(self):
+       # Initialize OpenAI API and browser
+       pass
+
+    def execute_voice_command(self):
+        # Read from audiototext file
+        # Call for an agent with command
+        # Run agent
+        # Return status (success or failure)
+        pass
+
+    def save_results(self):
+        # Log for results
+        # Save to a file
+        pass
