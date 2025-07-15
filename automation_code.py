a68ff7ae628569dd2e4ae9ca9bbf30063947fec0
Beks <b_abdimanapov@u.pacific.edu>
2025-07-15 12:06:17 -0700

    browser_use alpha engine made

--- automation_code.py
+++ automation_code.py
@@ -1,2 +1,2 @@
 import webbrowser
-webbrowser.open('https://www.youtube.com')
+webbrowser.open("https://www.youtube.com")
--- modules/browser-use_engine.py
+++ modules/browser-use_engine.py
@@ -1,18 +1,33 @@
 # Browser-use_engine - This module is used to control the browser using voice commands!
+import os
+import asyncio
+from dotenv import load_dotenv
+load_dotenv()
 from browser_use import Agent, Browser
 from browser_use.llm import ChatOpenAI
 
 class Engine:
     def __init__(self):
        # Initialize OpenAI API and browser
-       pass
+       self.llm = ChatOpenAI(model="gpt-4o-mini", api_key = os.getenv("OPENAI_API_KEY"))
+       self.browser = Browser()
 
-    def execute_voice_command(self):
+    async def execute_voice_command(self):
         # Read from audiototext file
+        try:
+            with open("audiototext.txt", "r") as file:
+                text = file.read()
+        except FileNotFoundError:
+            return "error: File not found"
+
         # Call for an agent with command
+        agent = Agent(task=text,llm=self.llm, browser = self.browser)
+
         # Run agent
+        await agent.run()
+
         # Return status (success or failure)
-        pass
+        return "Success"
 
     def save_results(self):
         # Log for results
